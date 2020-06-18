ee0ace1ea3c245eee61cf650e6f2a438aed39a4e
Alex <alex.gabriel@mail.ru>
2020-06-18 11:16:55 +0300

    feat: add useEffect hook and fetch words

--- src/components/word-builder/WordBuilderGame/WordBuilderGame.jsx
+++ src/components/word-builder/WordBuilderGame/WordBuilderGame.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './WordBuilderGame.scss';
 
-const WordBuilderGame = () => (
-  <div className="main-frame">test</div>
-);
+const WordBuilderGame = () => {
+  const [words, setWords] = useState([]);
 
+  useEffect(async () => {
+    const WORDS_URL = 'https://afternoon-falls-25894.herokuapp.com/words?page=0&group=0';
+    const data = await fetch(WORDS_URL);
+    const res = await data.json();
+    const wordsArray = res.map(({
+      audio, image, audioExample, textExample, transcription, word, wordTranslate,
+    }) => ({
+      audio, image, audioExample, textExample, transcription, word, wordTranslate,
+    }));
+    setWords(wordsArray);
+    console.log(wordsArray);
+  }, []);
+
+  return (
+    <div className="word-constructor-wrapper">{words.length}</div>
+  );
+};
 export default WordBuilderGame;
