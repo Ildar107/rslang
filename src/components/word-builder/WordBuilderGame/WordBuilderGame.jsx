3a326b740f6f4ef8ccb1e275a57ee8d41eaa5a94
Alex <alex.gabriel@mail.ru>
2020-06-18 12:50:26 +0300

    feat: wrap useEffect fetch in fetchData function and add some stying

--- src/components/word-builder/WordBuilderGame/WordBuilderGame.jsx
+++ src/components/word-builder/WordBuilderGame/WordBuilderGame.jsx
@@ -3,22 +3,34 @@ import './WordBuilderGame.scss';
 
 const WordBuilderGame = () => {
   const [words, setWords] = useState([]);
-
-  useEffect(async () => {
-    const WORDS_URL = 'https://afternoon-falls-25894.herokuapp.com/words?page=0&group=0';
-    const data = await fetch(WORDS_URL);
-    const res = await data.json();
-    const wordsArray = res.map(({
-      audio, image, audioExample, textExample, transcription, word, wordTranslate,
-    }) => ({
-      audio, image, audioExample, textExample, transcription, word, wordTranslate,
-    }));
-    setWords(wordsArray);
-    console.log(wordsArray);
+  // https://raw.githubusercontent.com/alexgabrielov/rslang-data/master/files/01_0009_example.mp3
+  useEffect(() => {
+    async function fetchData() {
+      const WORDS_URL = 'https://afternoon-falls-25894.herokuapp.com/words?page=0&group=0';
+      const data = await fetch(WORDS_URL);
+      const res = await data.json();
+      const wordsArray = res.map(({
+        audio, image, audioExample, textExample, transcription, word, wordTranslate,
+      }) => ({
+        audio: `https://raw.githubusercontent.com/alexgabrielov/rslang-data/master/${audio}`,
+        image: `https://raw.githubusercontent.com/alexgabrielov/rslang-data/master/${image}`,
+        audioExample: `https://raw.githubusercontent.com/alexgabrielov/rslang-data/master/${audioExample}`,
+        textExample,
+        transcription,
+        word,
+        wordTranslate,
+      }));
+      setWords(wordsArray);
+      console.log(wordsArray);
+    }
+    fetchData();
   }, []);
 
   return (
-    <div className="word-constructor-wrapper">{words.length}</div>
+    <div className="word-constructor-wrapper">
+      <div className="audio-button">PLAY</div>
+      <span className="eng-word">{words[0] && words[0].word}</span>
+    </div>
   );
 };
 export default WordBuilderGame;
