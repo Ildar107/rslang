b91865cc760dad22f035cdcaf4d7a23e0d4f91f6
Yauheni Shypila <evshipilo@gmail.com>
2020-06-19 17:51:47 +0300

    feat: add styles

--- webpack.config.js
+++ webpack.config.js
@@ -3,71 +3,71 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.jsx',
-    context: path.resolve(__dirname),
-    devtool: 'eval-source-map',
-  
-    resolve: {
-      extensions: ['.jsx', '.js'],
-      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    },
-  
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js',
-      publicPath: '/',
-    },
-  
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)?$/,
-          use: ['babel-loader'],
-          exclude: /node_modules/,
-        },
-        {
-          enforce: 'pre',
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          loader: 'eslint-loader',
-          options: {
-            fix: true,
-          },
-        },
-        {
-          test: /\.(css|scss)$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-            'file-loader',
-          ],
-        },
-      ],
-    },
+  entry: './src/main.jsx',
+  context: path.resolve(__dirname),
+  devtool: 'eval-source-map',
 
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: './src/index.html',
-        }),
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-            from: './src/assets/images/',
-            to: './images'
-            }    
-        ]
-      })
+  resolve: {
+    extensions: ['.jsx', '.js'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      // {
+      //   enforce: 'pre',
+      //   test: /\.(js|jsx)$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   options: {
+      //     fix: true,
+      //   },
+      // },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
+  },
 
-    devServer: {
-      contentBase: '/',
-      port: 8181,
-      historyApiFallback: true,
-      inline: true,
-      hot: true,
-      open: true,
-    },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './src/assets/images/',
+          to: './images'
+        }
+      ]
+    })
+  ],
+
+  devServer: {
+    contentBase: '/',
+    port: 8181,
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+    open: true,
+  },
 };
-    
+
