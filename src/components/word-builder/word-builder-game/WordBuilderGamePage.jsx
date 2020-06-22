8e3a7fe81950c7a668dc9f06e171df7342a9fe94
Alex <alex.gabriel@mail.ru>
2020-06-22 17:33:52 +0300

    feat: pass props down to game page

--- src/components/word-builder/word-builder-game/WordBuilderGamePage.jsx
+++ src/components/word-builder/word-builder-game/WordBuilderGamePage.jsx
@@ -1,11 +1,82 @@
-import React, {
-  useState, useEffect, useMemo,
-} from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
 
-const WordBuilderGamePage = (props) => {
-  // return (
+const WordBuilderGamePage = ({
+  currentWordObj,
+  currentWordIndex,
+  solved,
+  currentLetterIndex,
+  shuffledArray,
+  guessedLettersIndexes,
+  currentLetter,
+  setGuessedLettersIndexes,
+  setCurrentLetterIndex,
+  setSolved,
+  nextButtonHandler,
+}) => (
+  <>
+    <button type="button" className="audio-button" onClick={() => new Audio(currentWordObj.audio).play()}>s</button>
+    <div className="current-progress-div">{`${currentWordIndex + 1} / 20`}</div>
+    <span className="eng-word">{currentWordObj?.wordTranslate}</span>
 
-  // )
-};
+    {solved
+      ? <span className="transcription">{currentWordObj?.transcription}</span>
+      : <span className="rules-span">Собери слово из букв</span>}
+
+    <div className="letter-wrapper">
+      {currentWordObj?.word.split('')
+         .map((letter, index) => (
+           <div
+             key={`${letter}${index + 1}`}
+             className="empty-letter"
+           >
+             <span className={!solved && index >= currentLetterIndex ? 'hidden' : ''}>{letter}</span>
+           </div>
+         ))}
+    </div>
+
+    <div className="letter-wrapper">
+      {shuffledArray
+        .map((letter, index) => (
+          <button
+            key={`${letter}${index + 1}`}
+            className={`letter ${guessedLettersIndexes.includes(index) || solved ? 'hidden' : ''}`}
+            type="button"
+            onClick={() => {
+              if (letter === currentLetter) {
+                setGuessedLettersIndexes([...guessedLettersIndexes, index]);
+                setCurrentLetterIndex(currentLetterIndex + 1);
+                if (currentLetterIndex === currentWordObj?.word.length - 1) {
+                  setSolved(true);
+                }
+              }
+            }}
+          >
+            <span>{letter}</span>
+          </button>
+        ))}
+    </div>
+    {solved
+      && (
+      <img
+        src={currentWordObj?.image}
+        alt={currentWordObj?.word}
+        className="word-image"
+      />
+      )}
+    {solved
+      && <span className="context-span">Контекст</span>}
+    {solved
+      && <span className="text-example-span">{currentWordObj?.textExample}</span>}
+
+    <button
+      type="button"
+      className="next-button"
+      onMouseDown={nextButtonHandler}
+    >
+      {solved ? 'Далее' : 'Не знаю :('}
+    </button>
+  </>
+);
 
 export default WordBuilderGamePage;
--- src/components/word-builder/word-builder-main/WordBuilderMainPage.jsx
+++ src/components/word-builder/word-builder-main/WordBuilderMainPage.jsx
@@ -90,71 +90,20 @@ const WordBuilderMainPage = () => {
       {finished
         ? <WordBuilderStatsPage wordObjects={wordObjects} />
         : (
-          <>
-            <button type="button" className="audio-button" onClick={() => new Audio(currentWordObj.audio).play()}>s</button>
-            <div className="current-progress-div">{`${currentWordIndex + 1} / 20`}</div>
-            <span className="eng-word">{currentWordObj?.wordTranslate}</span>
-
-            {solved
-              ? <span className="transcription">{currentWordObj?.transcription}</span>
-              : <span className="rules-span">Собери слово из букв</span>}
-
-            <div className="letter-wrapper">
-              {currentWordObj?.word.split('')
-         .map((letter, index) => (
-           <div
-             key={`${letter}${index + 1}`}
-             className="empty-letter"
-           >
-             <span className={!solved && index >= currentLetterIndex ? 'hidden' : ''}>{letter}</span>
-           </div>
-         ))}
-            </div>
-
-            <div className="letter-wrapper">
-              {shuffledArray
-                .map((letter, index) => (
-                  <button
-                    key={`${letter}${index + 1}`}
-                    className={`letter ${guessedLettersIndexes.includes(index) || solved ? 'hidden' : ''}`}
-                    type="button"
-                    onClick={() => {
-                      if (letter === currentLetter) {
-                        setGuessedLettersIndexes([...guessedLettersIndexes, index]);
-                        setCurrentLetterIndex(currentLetterIndex + 1);
-                        if (currentLetterIndex === currentWordObj?.word.length - 1) {
-                          setSolved(true);
-                        }
-                      }
-                    }}
-                  >
-                    <span>{letter}</span>
-                  </button>
-                ))}
-            </div>
-            {solved
-      && (
-      <img
-        src={currentWordObj?.image}
-        alt={currentWordObj?.word}
-        className="word-image"
-      />
-      )}
-            {solved
-      && <span className="context-span">Контекст</span>}
-            {solved
-      && <span className="text-example-span">{currentWordObj?.textExample}</span>}
-
-            <button
-              type="button"
-              className="next-button"
-              onMouseDown={nextButtonHandler}
-            >
-              {solved ? 'Далее' : 'Не знаю :('}
-            </button>
-          </>
+          <WordBuilderGamePage
+            currentWordObj={currentWordObj}
+            currentWordIndex={currentWordIndex}
+            solved={solved}
+            currentLetterIndex={currentLetterIndex}
+            shuffledArray={shuffledArray}
+            guessedLettersIndexes={guessedLettersIndexes}
+            currentLetter={currentLetter}
+            setGuessedLettersIndexes={setGuessedLettersIndexes}
+            setCurrentLetterIndex={setCurrentLetterIndex}
+            setSolved={setSolved}
+            nextButtonHandler={nextButtonHandler}
+          />
         )}
-
     </div>
   );
 };
