9970e1a269725c3eaf231fe98645bac8d2565201
Alex <alex.gabriel@mail.ru>
2020-06-23 18:28:38 +0300

    feat: add some bootstrap buttons

--- src/components/word-builder/word-builder-game/WordBuilderGamePage.jsx
+++ src/components/word-builder/word-builder-game/WordBuilderGamePage.jsx
@@ -16,7 +16,13 @@ const WordBuilderGamePage = ({
 }) => (
   <>
     {solved
-    && <button type="button" className="audio-button" onClick={() => new Audio(currentWordObj.audio).play()}>s</button>}
+    && (
+    <button type="button" className="audio-button-solved btn btn-primary" onClick={() => new Audio(currentWordObj.audio).play()}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <path fill="currentColor" d="M15.788 13.007a3 3 0 110 5.985c.571 3.312 2.064 5.675 3.815 5.675 2.244 0 4.064-3.88 4.064-8.667 0-4.786-1.82-8.667-4.064-8.667-1.751 0-3.244 2.363-3.815 5.674zM19 26c-3.314 0-12-4.144-12-10S15.686 6 19 6s6 4.477 6 10-2.686 10-6 10z" fillRule="evenodd" />
+      </svg>
+    </button>
+    )}
     <div className="current-progress-div">{`${currentWordIndex + 1} / 10`}</div>
     <span className="eng-word">{currentWordObj?.wordTranslate}</span>
 
@@ -72,7 +78,7 @@ const WordBuilderGamePage = ({
 
     <button
       type="button"
-      className="next-button"
+      className={`next-button btn btn-secondary ${solved && 'btn-success'}`}
       onMouseDown={nextButtonHandler}
     >
       {solved ? 'Далее' : 'Не знаю :('}
--- src/components/word-builder/word-builder-stats/WordBuilderStatsPage.jsx
+++ src/components/word-builder/word-builder-stats/WordBuilderStatsPage.jsx
@@ -19,10 +19,12 @@ const WordBuilderStatsPage = ({ wordObjects }) => (
               <div>
                 <button
                   type="button"
-                  className="audio-button-stats"
+                  className="audio-button btn btn-primary"
                   onClick={() => new Audio(audio).play()}
                 >
-                  s
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                    <path fill="currentColor" d="M15.788 13.007a3 3 0 110 5.985c.571 3.312 2.064 5.675 3.815 5.675 2.244 0 4.064-3.88 4.064-8.667 0-4.786-1.82-8.667-4.064-8.667-1.751 0-3.244 2.363-3.815 5.674zM19 26c-3.314 0-12-4.144-12-10S15.686 6 19 6s6 4.477 6 10-2.686 10-6 10z" fillRule="evenodd" />
+                  </svg>
                 </button>
                 <span>
                   {word}
@@ -33,7 +35,6 @@ const WordBuilderStatsPage = ({ wordObjects }) => (
                   {wordTranslate}
                 </span>
               </div>
-
             </li>
           ))}
       </ul>
@@ -55,10 +56,12 @@ const WordBuilderStatsPage = ({ wordObjects }) => (
               <div>
                 <button
                   type="button"
-                  className="audio-button-stats"
+                  className="audio-button btn btn-primary"
                   onClick={() => new Audio(audio).play()}
                 >
-                  s
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                    <path fill="currentColor" d="M15.788 13.007a3 3 0 110 5.985c.571 3.312 2.064 5.675 3.815 5.675 2.244 0 4.064-3.88 4.064-8.667 0-4.786-1.82-8.667-4.064-8.667-1.751 0-3.244 2.363-3.815 5.674zM19 26c-3.314 0-12-4.144-12-10S15.686 6 19 6s6 4.477 6 10-2.686 10-6 10z" fillRule="evenodd" />
+                  </svg>
                 </button>
                 <span>
                   {word}
