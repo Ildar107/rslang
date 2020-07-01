d98788a044dbf84b5a7d9a7d0529a87bfad29e48
Alex <alex.gabriel@mail.ru>
2020-07-01 17:40:14 +0300

    feat: add hrefs for games and wrap minigames in skeleton

--- src/components/minigame/MiniGame.jsx
+++ src/components/minigame/MiniGame.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-restricted-globals */
 import React from 'react';
 import './MiniGame.scss';
 
 const MiniGame = ({
-  name, description, imageSrc,
+  name, description, imageSrc, ref,
 }) => (
-  <div className="card">
+  <div
+    className="card"
+    onClick={() => {
+      location.href = ref;
+    }}
+  >
     <img className="card-img" src={imageSrc} alt="" />
     <div className="card-img-overlay">
       <h3 className="card-title">
--- src/pages/minigames/MiniGamesPage.jsx
+++ src/pages/minigames/MiniGamesPage.jsx
@@ -1,53 +1,70 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import Header from '../../components/header/Header';
+// import { Container } from 'react-bootstrap';
+// import Header from '../../components/header/Header';
 import MiniGame from '../../components/minigame/MiniGame';
+import Skeleton from '../../components/skeleton/Skeleton';
+import routes from '../../constants/routes';
+
+const {
+  SPEAKIT,
+  WORD_BUILDER,
+  PUZZLE,
+  AUDIOCALL,
+  SAVANNAH,
+} = routes;
 
 const miniGamesData = [{
   name: 'SpeakIt',
   description: 'Улучшает восприятие английской речи на слух и произношение',
   imageSrc: '/images/mini-igri-images/speakit.jpg',
+  ref: SPEAKIT,
 }, {
   name: 'English Puzzle',
   description: 'Развивает восприятие английской речи на слух',
   imageSrc: '/images/mini-igri-images/engpuzzle.jpg',
+  ref: PUZZLE,
 }, {
   name: 'Саванна',
   description: 'Оттачивает понимание английской речи и быстрого перевода слов, помогает не забыть выученные слова',
   imageSrc: '/images/mini-igri-images/savannah.jpg',
+  ref: SAVANNAH,
 }, {
   name: 'Аудиовызов',
   description: 'Улучшает восприятие английской речи на слух',
   imageSrc: '/images/mini-igri-images/audiovizov.jpg',
+  ref: AUDIOCALL,
 }, {
   name: 'Спринт',
   description: 'Учит быстро переводить с английского на русский язык',
   imageSrc: '/images/mini-igri-images/sprint.jpg',
+  ref: '',
 }, {
   name: 'Конструктор Слов',
   description: 'Формирует навыки обратного перевода и написания английских слов',
   imageSrc: '/images/mini-igri-images/wordbuilder.jpg',
+  ref: WORD_BUILDER,
 }];
 
 const MiniGamesPage = () => (
-  <>
-    <Header />
-    <Container fluid className="mini-games">
-      <ul>
-        {miniGamesData.map(({
-          name, description, imageSrc,
-        }) => (
-          <li key={name}>
-            <MiniGame
-              name={name}
-              description={description}
-              imageSrc={imageSrc}
-            />
-          </li>
-        ))}
-      </ul>
-    </Container>
-  </>
+  <Skeleton wrapperClass="mini-games" title="Мини-игры">
+    {/* <Header /> */}
+    {/* <Container fluid className="mini-games"> */}
+    <ul>
+      {miniGamesData.map(({
+        name, description, imageSrc, ref,
+      }) => (
+        <li key={name}>
+          <MiniGame
+            name={name}
+            description={description}
+            imageSrc={imageSrc}
+            ref={ref}
+          />
+        </li>
+      ))}
+    </ul>
+    {/* </Container> */}
+  </Skeleton>
 );
 
 export default MiniGamesPage;
