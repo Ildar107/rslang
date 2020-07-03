76c45e20e17e6aebb395da0ee3929b665d5bae42
Yauheni Shypila <evshipilo@gmail.com>
2020-07-03 03:24:12 +0300

    feat: add start game page

--- src/pages/startGame/startGamePage.jsx
+++ src/pages/startGame/startGamePage.jsx
@@ -3,13 +3,38 @@ import React, { useState } from 'react';
 // import Skeleton from '../../components/skeleton/Skeleton';
 import './startGamePage.scss';
 import { Button, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import * as qs from 'query-string';
 import EndGameModal from '../../components/endGameModal/endGameModal';
 import routes from '../../constants/routes';
 
+const description = {
+  PUZZLE: {
+    name: 'PUZZLE',
+    info: 'Собирай предложения из слов, используй'
+        + ' подсказки, сложи паззл из предложений.',
+  },
+  SPEAKIT: {
+    name: 'SPEAK IT',
+    info: 'Слушай'
+        + ' слова, читай транскрипцию, добейся'
+        + ' правильного произношения.',
+  },
+  SAVANNAH: {
+    name: 'SAVANNAH',
+    info: '',
+  },
+  AUDIOCALL: {
+    name: 'AUDIOCALL',
+    info: '',
+  },
+};
+
 function StartGamePage() {
   const [modalShow, setModalShow] = useState(false);
-  // const { game } = props;
+
+  const game = qs.parse(useLocation().search).q;
+  const path = routes[game];
   return (
     <Container fluid>
       <div className="start-game">
@@ -19,13 +44,17 @@ function StartGamePage() {
         >
           <i className="material-icons">close</i>
         </Button>
-        <Link to={routes.PUZZLE}>
-          <Button
-            className="start-game-start-btn"
-          >
-            Начать игру
-          </Button>
-        </Link>
+        <div className="start-game-description">
+          <h2>{description[game].name}</h2>
+          <p>{description[game].info}</p>
+          <Link to={path}>
+            <Button
+              className="start-game-start-btn"
+            >
+              Начать игру
+            </Button>
+          </Link>
+        </div>
         <EndGameModal
           show={modalShow}
           onHide={() => setModalShow(false)}
