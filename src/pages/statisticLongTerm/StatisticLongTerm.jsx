27a95122b1f2fd54e1009b75d7822276abfe082a
Yauheni Shypila <evshipilo@gmail.com>
2020-07-04 21:18:16 +0300

    feat: add line chart

--- src/pages/statisticLongTerm/StatisticLongTerm.jsx
+++ src/pages/statisticLongTerm/StatisticLongTerm.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
+import Skeleton from '../../components/skeleton/Skeleton';
 
 const testDATA = [
   { date: '1/6/2020', words: 6 },
@@ -14,9 +16,38 @@ const testDATA = [
   { date: '11/6/2020', words: 5 },
 ];
 
+const data = {
+  labels: testDATA.map((item) => item.date),
+  datasets: [
+    {
+      label: 'Количество изученных слов по датам.',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: testDATA.map((item) => item.words),
+    },
+  ],
+};
+
 function StatisticLongTerm() {
   return (
-    <> sdcsdcsdcsdc</>
+    <Skeleton wrapperClass="statistic-long-term-page" title="Статистика">
+      <Line data={data} />
+    </Skeleton>
   );
 }
 
