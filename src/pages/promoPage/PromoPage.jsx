241c30d08675c74c43ba2cb2e305c5b7fcff2ab7
Lashkevich Alexandr <zanchesssss@gmail.com>
2020-07-11 13:39:45 +0300

    init: start promo page task

---
+++ src/pages/promoPage/PromoPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './promo-page.scss';
+import Skeleton from '../../components/skeleton/Skeleton';
+
+const PromoPage = () => (
+  <Skeleton wrapperClass="main-page">
+    <h1 className="team__header">Описание приложения :</h1>
+  </Skeleton>
+);
+export default PromoPage;
