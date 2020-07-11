dd5e03af06e77db0cd50c5ee706b1a190441db17
Lashkevich Alexandr <zanchesssss@gmail.com>
2020-07-12 01:27:06 +0300

    feat: add first description

--- src/pages/promoPage/PromoPage.jsx
+++ src/pages/promoPage/PromoPage.jsx
@@ -13,83 +13,127 @@ import {
   mainDescription,
 } from './promo-data';
 
-const PromoPage = () => (
-  <div>
-    <Skeleton wrapperClass="main-page">
-      <Container className="promo" fluid>
-        <h1 className="team__header">Описание приложения</h1>
+const PromoPage = () => {
+  const styleBorder = {
+    border: '1px solid #d1d1d1',
+  };
 
-        <Row className="promo__description">
-          <h2>{appDescription.header}</h2>
-        </Row>
-        <Row>
-          <p className="promo__description">{appDescription.description}</p>
-        </Row>
+  const styleCursor = {
+    cursor: 'pointer',
+  };
 
-        <Accordion>
-          <Card style={{
-            border: '1px solid #d1d1d1',
-          }}
-          >
-            <Accordion.Toggle
-              style={{
-                cursor: 'pointer',
-              }}
-              as={Card.Header}
-              eventKey="0"
-            >
-              <h3 className="promo__h3">{mainDescription.header}</h3>
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>
-                <div>
-                  <p>
-                    {mainDescription.description}
-                  </p>
-                </div>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="1">
-              Click me!
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey="1">
-              <Card.Body>Hello! I'm another body</Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+  return (
+    <div>
+      <Skeleton wrapperClass="main-page">
+        <Container className="promo" fluid>
+          <h1 className="team__header">Описание приложения</h1>
 
-        <Row>
-          <h2>{learnWordsDescription.header}</h2>
-        </Row>
-        <Row>
-          <h2>{learnWordsDescription.description}</h2>
-        </Row>
+          <Row>
+            <h2>{appDescription.header}</h2>
+          </Row>
+          <Row>
+            <p className="promo__description">{appDescription.description}</p>
+          </Row>
 
-        <Row>
-          <h2>{settingsDescription.header}</h2>
-        </Row>
-        <Row>
-          <h2>{settingsDescription.description}</h2>
-        </Row>
+          <Accordion>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="0"
+              >
+                <h3 className="promo__h3">{mainDescription.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <div className="promo__body">
+                    <p className="promo__description_l">
+                      {mainDescription.description}
+                    </p>
+                    <img className="promo__img" src="./images/promo-images/mainpage.jpg" alt="main page" />
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="2"
+              >
+                <h3 className="promo__h3">{mainDescription.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>
+                  <div className="promo__body">
+                    <p>
+                      {mainDescription.description}
+                    </p>
+                    <img className="promo__img" src="./images/promo-images/mainpage.jpg" alt="main page" />
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="3"
+              >
+                <h3 className="promo__h3">{mainDescription.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="3">
+                <Card.Body>
+                  <div className="promo__body">
+                    <p>
+                      {mainDescription.description}
+                    </p>
+                    <img className="promo__img" src="./images/promo-images/mainpage.jpg" alt="main page" />
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="1">
+                Click me!
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>Hello! another body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
 
-        <Row>
-          <h2>{miniGamesDescription.header}</h2>
-        </Row>
-        <Row>
-          <h2>{miniGamesDescription.description}</h2>
-        </Row>
+          <Row>
+            <h2>{learnWordsDescription.header}</h2>
+          </Row>
+          <Row>
+            <h2>{learnWordsDescription.description}</h2>
+          </Row>
 
-        <Row>
-          <h2>{statisticPageDescription.header}</h2>
-        </Row>
-        <Row>
-          <h2>{statisticPageDescription.description}</h2>
-        </Row>
-      </Container>
+          <Row>
+            <h2>{settingsDescription.header}</h2>
+          </Row>
+          <Row>
+            <h2>{settingsDescription.description}</h2>
+          </Row>
 
-    </Skeleton>
-  </div>
-);
+          <Row>
+            <h2>{miniGamesDescription.header}</h2>
+          </Row>
+          <Row>
+            <h2>{miniGamesDescription.description}</h2>
+          </Row>
+
+          <Row>
+            <h2>{statisticPageDescription.header}</h2>
+          </Row>
+          <Row>
+            <h2>{statisticPageDescription.description}</h2>
+          </Row>
+        </Container>
+
+      </Skeleton>
+    </div>
+  );
+};
 export default PromoPage;
