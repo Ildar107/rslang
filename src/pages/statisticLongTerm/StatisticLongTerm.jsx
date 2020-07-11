b1d40e7d60626423c67e43347f5438f4235140c3
Alex <alex.gabriel@mail.ru>
2020-07-11 12:45:01 +0300

    feat: put all code inside statisticLongTerm component

--- src/pages/statisticLongTerm/StatisticLongTerm.jsx
+++ src/pages/statisticLongTerm/StatisticLongTerm.jsx
@@ -5,140 +5,146 @@ import Skeleton from '../../components/skeleton/Skeleton';
 import './statisticLongTerm.scss';
 import userWordsService from '../../services/user.words.services';
 
-const testDATA = [
-  { date: '1/6/2020', words: 6 },
-  { date: '2/6/2020', words: 3 },
-  { date: '3/6/2020', words: 1 },
-  { date: '4/6/2020', words: 16 },
-  { date: '5/6/2020', words: 10 },
-  { date: '6/6/2020', words: 8 },
-  { date: '7/6/2020', words: 3 },
-  { date: '8/6/2020', words: 12 },
-  { date: '9/6/2020', words: 14 },
-  { date: '10/6/2020', words: 16 },
-  { date: '11/6/2020', words: 5 },
-];
+const { getUserWords } = userWordsService;
 
-const dates = testDATA.map((item) => item.date);
-const numOfWords = testDATA.map((item) => item.words);
-const sumOfWords = [];
-const sum = numOfWords.reduce((acc, item) => {
-  sumOfWords.push(acc + item);
-  return (acc + item);
-});
-sumOfWords.push(sum);
-const left = sumOfWords.map((it) => 3600 - it);
-const percent = sumOfWords.map((it) => ((100 * it) / 3600).toFixed(1));
+const StatisticLongTerm = () => {
+  const testDATA = [
+    { date: '1/6/2020', words: 6 },
+    { date: '2/6/2020', words: 3 },
+    { date: '3/6/2020', words: 1 },
+    { date: '4/6/2020', words: 16 },
+    { date: '5/6/2020', words: 10 },
+    { date: '6/6/2020', words: 8 },
+    { date: '7/6/2020', words: 3 },
+    { date: '8/6/2020', words: 12 },
+    { date: '9/6/2020', words: 14 },
+    { date: '10/6/2020', words: 16 },
+    { date: '11/6/2020', words: 5 },
+  ];
 
-const data = {
-  labels: dates,
-  datasets: [
-    {
-      label: 'Количество изученных слов.',
-      fill: true,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(37,95,82,0.47)',
-      borderColor: 'rgba(37,95,82,0.47)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: '#000000',
-      pointBackgroundColor: '#000000',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgb(236,9,9)',
-      pointHoverBorderColor: 'rgb(236,9,9)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 2,
-      pointHitRadius: 10,
-      data: numOfWords,
-    },
-  ],
-};
+  const dates = testDATA.map((item) => item.date);
+  const numOfWords = testDATA.map((item) => item.words);
+  const sumOfWords = [];
+  const sum = numOfWords.reduce((acc, item) => {
+    sumOfWords.push(acc + item);
+    return (acc + item);
+  });
+  sumOfWords.push(sum);
+  const left = sumOfWords.map((it) => 3600 - it);
+  const percent = sumOfWords.map((it) => ((100 * it) / 3600).toFixed(1));
 
-const data1 = {
-  labels: dates,
-  datasets: [
-    {
-      label: 'Общее количество изученных слов.',
-      fill: true,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(37,70,95,0.47)',
-      borderColor: 'rgba(37,70,95,0.47)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: '#000000',
-      pointBackgroundColor: '#000000',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgb(236,9,9)',
-      pointHoverBorderColor: 'rgb(236,9,9)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 2,
-      pointHitRadius: 10,
-      data: sumOfWords,
-    },
-  ],
-};
+  const data = {
+    labels: dates,
+    datasets: [
+      {
+        label: 'Количество изученных слов.',
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(37,95,82,0.47)',
+        borderColor: 'rgba(37,95,82,0.47)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: '#000000',
+        pointBackgroundColor: '#000000',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgb(236,9,9)',
+        pointHoverBorderColor: 'rgb(236,9,9)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 2,
+        pointHitRadius: 10,
+        data: numOfWords,
+      },
+    ],
+  };
 
-const data2 = {
-  labels: dates,
-  datasets: [
-    {
-      label: 'Процент изученных слов.',
-      fill: true,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(71,37,95,0.47)',
-      borderColor: 'rgba(71,37,95,0.47)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: '#000000',
-      pointBackgroundColor: '#000000',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgb(236,9,9)',
-      pointHoverBorderColor: 'rgb(236,9,9)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 2,
-      pointHitRadius: 10,
-      data: percent,
-    },
-  ],
-};
+  const data1 = {
+    labels: dates,
+    datasets: [
+      {
+        label: 'Общее количество изученных слов.',
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(37,70,95,0.47)',
+        borderColor: 'rgba(37,70,95,0.47)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: '#000000',
+        pointBackgroundColor: '#000000',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgb(236,9,9)',
+        pointHoverBorderColor: 'rgb(236,9,9)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 2,
+        pointHitRadius: 10,
+        data: sumOfWords,
+      },
+    ],
+  };
 
-const data3 = {
-  labels: dates,
-  datasets: [
-    {
-      label: 'Осталось изучить.',
-      fill: true,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(84,95,37,0.47)',
-      borderColor: 'rgba(84,95,37,0.47)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: '#000000',
-      pointBackgroundColor: '#000000',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgb(236,9,9)',
-      pointHoverBorderColor: 'rgb(236,9,9)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 2,
-      pointHitRadius: 10,
-      data: left,
-    },
-  ],
-};
+  const data2 = {
+    labels: dates,
+    datasets: [
+      {
+        label: 'Процент изученных слов.',
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(71,37,95,0.47)',
+        borderColor: 'rgba(71,37,95,0.47)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: '#000000',
+        pointBackgroundColor: '#000000',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgb(236,9,9)',
+        pointHoverBorderColor: 'rgb(236,9,9)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 2,
+        pointHitRadius: 10,
+        data: percent,
+      },
+    ],
+  };
 
-function StatisticLongTerm() {
+  const data3 = {
+    labels: dates,
+    datasets: [
+      {
+        label: 'Осталось изучить.',
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(84,95,37,0.47)',
+        borderColor: 'rgba(84,95,37,0.47)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: '#000000',
+        pointBackgroundColor: '#000000',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgb(236,9,9)',
+        pointHoverBorderColor: 'rgb(236,9,9)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 2,
+        pointHitRadius: 10,
+        data: left,
+      },
+    ],
+  };
+
+  const { userId, JWT: jwt } = localStorage;
+  const userWords = getUserWords(jwt, userId).then((res) => console.log(res));
+  // console.log(userWords);
+
   return (
     <Skeleton wrapperClass="statistic-long-term-page" title="Статистика">
       <Row>
@@ -167,6 +173,6 @@ function StatisticLongTerm() {
       </Row>
     </Skeleton>
   );
-}
+};
 
 export default StatisticLongTerm;
