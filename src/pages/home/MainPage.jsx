84bbc8b30044b46bb4d523be4f6bed135a3879cf
Alex <alex.gabriel@mail.ru>
2020-07-12 15:34:23 +0300

    feat: add info to main page

--- src/pages/home/MainPage.jsx
+++ src/pages/home/MainPage.jsx
@@ -11,18 +11,41 @@ const MainPage = () => (
   <Skeleton wrapperClass="main-page" title="Главная">
     <Row>
       <Col lg={6}>
-        <Tilebox title="Новые слова" value="50" updateValue="+50" isUp icon="uil-search" />
+        <Tilebox
+          title="Новые слова"
+          value={JSON.parse(localStorage.getItem('userSettings')).wordsPerDay}
+          updateValue="+50"
+          isUp
+          icon="uil-search"
+        />
       </Col>
       <Col lg={6}>
-        <Tilebox title="Слова для практики" value="50" updateValue="+20" isUp icon="uil-history-alt" />
+        <Tilebox
+          title="Слова для практики"
+          value={JSON.parse(localStorage.getItem('userSettings')).cardsPerDay}
+          updateValue="+20"
+          isUp
+          icon="uil-history-alt"
+        />
       </Col>
     </Row>
     <Row>
       <Col lg={6}>
-        <Tilebox title="Пройдено слов по практике" value="20" updateValue="+20" isUp icon="uil-top-arrow-to-top" />
+        <Tilebox
+          title="Пройдено слов по практике"
+          value={localStorage.getItem('currentWordIndex') ? localStorage.getItem('currentWordIndex') : 0}
+          updateValue="+20"
+          isUp
+          icon="uil-top-arrow-to-top"
+        />
       </Col>
       <Col lg={6}>
-        <Tilebox title="Лучшая серия" value="10" updateValue="-2" icon="uil-medal" />
+        <Tilebox
+          title="Лучшая серия"
+          value={localStorage.getItem('longestStreak') ? +localStorage.getItem('longestStreak') + 1 : 0}
+          updateValue="-2"
+          icon="uil-medal"
+        />
       </Col>
     </Row>
     <Row>
