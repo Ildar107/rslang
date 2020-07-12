bcd5670bcfcc5f32462221628ca8621aad3df313
Alex <alex.gabriel@mail.ru>
2020-07-12 19:56:18 +0300

    feat: add main page info changes

--- src/pages/home/MainPage.jsx
+++ src/pages/home/MainPage.jsx
@@ -12,6 +12,13 @@ const { getUserWords } = userWordsService;
 
 const MainPage = () => {
   const [wordCount, setWordCount] = useState([]);
+  const [prevWordsPerDay] = useState(+localStorage.getItem('prevWordsPerDay') || 0);
+  const [prevCardsPerDay] = useState(+localStorage.getItem('prevCardsPerDay') || 0);
+  const [prevCurrentWordIndex] = useState(+localStorage.getItem('prevCurrentWordIndex') || 0);
+  const [prevLongestStreak] = useState(+localStorage.getItem('prevLongestStreak') || 0);
+
+  // console.log(prevWordsPerDay);
+  // console.log(JSON.parse(localStorage.getItem('userSettings')).wordsPerDay);
 
   useEffect(() => {
     async function fetchWords() {
@@ -20,7 +27,27 @@ const MainPage = () => {
       setWordCount(data.length);
     }
     fetchWords();
-  });
+  }, []);
+  useEffect(() => {
+    const { wordsPerDay, cardsPerDay } = JSON.parse(localStorage.getItem('userSettings'));
+    const { currentWordIndex, longestStreak } = localStorage;
+
+    console.log(currentWordIndex, longestStreak);
+
+    return () => {
+      localStorage.setItem('prevWordsPerDay', wordsPerDay);
+      localStorage.setItem('prevCardsPerDay', cardsPerDay);
+      localStorage.setItem('prevCurrentWordIndex', currentWordIndex);
+      localStorage.setItem('prevLongestStreak', longestStreak);
+      console.log('unmounted');
+    };
+  }, []);
+
+  const wordCountDifference = JSON.parse(localStorage.getItem('userSettings')).wordsPerDay - prevWordsPerDay;
+  const cardCountDifference = JSON.parse(localStorage.getItem('userSettings')).cardsPerDay - prevCardsPerDay;
+  const indexDifference = localStorage.getItem('currentWordIndex') - prevCurrentWordIndex;
+  const longestStreakDifference = localStorage.getItem('longestStreak') - prevLongestStreak;
+
   return (
     <Skeleton wrapperClass="main-page" title="Главная">
       <Row>
@@ -28,8 +55,8 @@ const MainPage = () => {
           <Tilebox
             title="Новые слова"
             value={JSON.parse(localStorage.getItem('userSettings')).wordsPerDay}
-            updateValue="+50"
-            isUp
+            updateValue={wordCountDifference}
+            isUp={wordCountDifference >= 0}
             icon="uil-search"
           />
         </Col>
@@ -37,8 +64,8 @@ const MainPage = () => {
           <Tilebox
             title="Слова для практики"
             value={JSON.parse(localStorage.getItem('userSettings')).cardsPerDay}
-            updateValue="+20"
-            isUp
+            updateValue={cardCountDifference}
+            isUp={cardCountDifference >= 0}
             icon="uil-history-alt"
           />
         </Col>
@@ -48,8 +75,8 @@ const MainPage = () => {
           <Tilebox
             title="Пройдено слов по практике"
             value={localStorage.getItem('currentWordIndex') ? localStorage.getItem('currentWordIndex') : 0}
-            updateValue="+20"
-            isUp
+            updateValue={indexDifference}
+            isUp={indexDifference >= 0}
             icon="uil-top-arrow-to-top"
           />
         </Col>
@@ -57,7 +84,8 @@ const MainPage = () => {
           <Tilebox
             title="Лучшая серия"
             value={+localStorage.getItem('longestStreak') ? +localStorage.getItem('longestStreak') + 1 : 0}
-            updateValue="-2"
+            updateValue={longestStreakDifference ? longestStreakDifference + 1 : 0}
+            isUp={longestStreakDifference >= 0}
             icon="uil-medal"
           />
         </Col>
