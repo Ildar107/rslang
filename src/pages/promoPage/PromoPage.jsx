9bb21a9c22bf71c6f9c0b0a16df7280064f5a983
Lashkevich Alexandr <zanchesssss@gmail.com>
2020-07-12 13:22:00 +0300

    feat: add promo content

--- src/pages/promoPage/PromoPage.jsx
+++ src/pages/promoPage/PromoPage.jsx
@@ -50,7 +50,30 @@ const PromoPage = () => {
                     <p className="promo__description_l">
                       {mainDescription.description}
                     </p>
-                    <img className="promo__img" src="./images/promo-images/mainpage.jpg" alt="main page" />
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/mainpage.jpg" alt="main page" />
+                    </div>
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="1"
+              >
+                <h3 className="promo__h3">{settingsDescription.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>
+                  <div className="promo__body">
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/settings.jpg" alt="main page" />
+                    </div>
+                    <p className="promo__description_l">
+                      {settingsDescription.description}
+                    </p>
                   </div>
                 </Card.Body>
               </Accordion.Collapse>
@@ -61,15 +84,17 @@ const PromoPage = () => {
                 as={Card.Header}
                 eventKey="2"
               >
-                <h3 className="promo__h3">{mainDescription.header}</h3>
+                <h3 className="promo__h3">{learnWordsDescription.header}</h3>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="2">
                 <Card.Body>
                   <div className="promo__body">
-                    <p>
-                      {mainDescription.description}
+                    <p className="promo__description_l">
+                      {learnWordsDescription.description}
                     </p>
-                    <img className="promo__img" src="./images/promo-images/mainpage.jpg" alt="main page" />
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/pagewithcards.jpg" alt="main page" />
+                    </div>
                   </div>
                 </Card.Body>
               </Accordion.Collapse>
@@ -80,56 +105,148 @@ const PromoPage = () => {
                 as={Card.Header}
                 eventKey="3"
               >
-                <h3 className="promo__h3">{mainDescription.header}</h3>
+                <h3 className="promo__h3">{miniGamesDescription.header}</h3>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="3">
                 <Card.Body>
                   <div className="promo__body">
-                    <p>
-                      {mainDescription.description}
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/Minigame.jpg" alt="main page" />
+                    </div>
+                    <p className="promo__description_l">
+                      {miniGamesDescription.description}
                     </p>
-                    <img className="promo__img" src="./images/promo-images/mainpage.jpg" alt="main page" />
                   </div>
                 </Card.Body>
               </Accordion.Collapse>
             </Card>
-            <Card>
-              <Accordion.Toggle as={Card.Header} eventKey="1">
-                Click me!
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="4"
+              >
+                <h3 className="promo__h3">{miniGamesDescription.AUDIO_CALL.header}</h3>
               </Accordion.Toggle>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>Hello! another body</Card.Body>
+              <Accordion.Collapse eventKey="4">
+                <Card.Body>
+                  <div className="promo__body">
+                    <p className="promo__description_l">
+                      {miniGamesDescription.AUDIO_CALL.description}
+                    </p>
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/audiocall.jpg" alt="main page" />
+                    </div>
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="5"
+              >
+                <h3 className="promo__h3">{miniGamesDescription.ENGLISH_PUZZLE.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="5">
+                <Card.Body>
+                  <div className="promo__body">
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/puzzle.jpg" alt="main page" />
+                    </div>
+                    <p className="promo__description_l">
+                      {miniGamesDescription.ENGLISH_PUZZLE.description}
+                    </p>
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="6"
+              >
+                <h3 className="promo__h3">{miniGamesDescription.SPEAKIT.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="6">
+                <Card.Body>
+                  <div className="promo__body">
+                    <p className="promo__description_l">
+                      {miniGamesDescription.SPEAKIT.description}
+                    </p>
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/speakit.jpg" alt="main page" />
+                    </div>
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="7"
+              >
+                <h3 className="promo__h3">{miniGamesDescription.SAVANNAH.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="7">
+                <Card.Body>
+                  <div className="promo__body">
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/savannah.jpg" alt="main page" />
+                    </div>
+                    <p className="promo__description_l">
+                      {miniGamesDescription.SAVANNAH.description}
+                    </p>
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="8"
+              >
+                <h3 className="promo__h3">{miniGamesDescription.SPRINT.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="8">
+                <Card.Body>
+                  <div className="promo__body">
+                    <p className="promo__description_l">
+                      {miniGamesDescription.SPRINT.description}
+                    </p>
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/sprint.jpg" alt="main page" />
+                    </div>
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card style={styleBorder}>
+              <Accordion.Toggle
+                style={styleCursor}
+                as={Card.Header}
+                eventKey="9"
+              >
+                <h3 className="promo__h3">{miniGamesDescription.WORD_BUILDER.header}</h3>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="9">
+                <Card.Body>
+                  <div className="promo__body">
+                    <div className="img-wrap">
+                      <img className="promo__img" src="./images/promo-images/constructor.jpg" alt="main page" />
+                    </div>
+                    <p className="promo__description_l">
+                      {miniGamesDescription.WORD_BUILDER.description}
+                    </p>
+                  </div>
+                </Card.Body>
               </Accordion.Collapse>
             </Card>
           </Accordion>
-
-          <Row>
-            <h2>{learnWordsDescription.header}</h2>
-          </Row>
-          <Row>
-            <h2>{learnWordsDescription.description}</h2>
-          </Row>
-
-          <Row>
-            <h2>{settingsDescription.header}</h2>
-          </Row>
-          <Row>
-            <h2>{settingsDescription.description}</h2>
-          </Row>
-
-          <Row>
-            <h2>{miniGamesDescription.header}</h2>
-          </Row>
-          <Row>
-            <h2>{miniGamesDescription.description}</h2>
-          </Row>
-
-          <Row>
-            <h2>{statisticPageDescription.header}</h2>
-          </Row>
-          <Row>
-            <h2>{statisticPageDescription.description}</h2>
-          </Row>
         </Container>
 
       </Skeleton>
